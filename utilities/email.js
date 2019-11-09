55344360f36c87b3b353d6f7c712b39179cfdb05
Nan Qin <qinnan0416@gmail.com>
2019-11-09 01:48:43 -0500

    add empty secret config

--- utilities/email.js
+++ utilities/email.js
@@ -5,3 +5,18 @@ function send_email(recipient_addr, email, callback){
     callback(new_err);
 }
 module.exports.send_email = send_email;
+
+var API_KEY = 'YOUR_API_KEY';
+var DOMAIN = 'YOUR_DOMAIN_NAME';
+var mailgun = require('mailgun-js')({apiKey: API_KEY, domain: DOMAIN});
+
+const data = {
+  from: 'Excited User <me@samples.mailgun.org>',
+  to: 'foo@example.com, bar@example.com',
+  subject: 'Hello',
+  text: 'Testing some Mailgun awesomeness!'
+};
+
+mailgun.messages().send(data, (error, body) => {
+  console.log(body);
+});
